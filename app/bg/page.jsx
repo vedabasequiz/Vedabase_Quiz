5a62629314f0fec40f9e1b286d2ec177f2e1068c
vedabasequiz <prakash.chincholikar@hotmail.com>
2026-02-04 08:28:25 -0600

    Style: Add back link at bottom of BG selector page for consistency

--- app/bg/page.jsx
+++ app/bg/page.jsx
@@ -40,6 +40,9 @@ export default function BgSelectorPage({ searchParams }) {
           </div>
         </Link>
       </div>
+      <div style={{ marginTop: 18 }}>
+        <Link href="/" className="backLink">&larr; Back</Link>
+      </div>
     </main>
   );
 }
